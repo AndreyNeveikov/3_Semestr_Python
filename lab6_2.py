37a1f1c64e9df7c347b4bb7213250be4f7303041
Andrey <alfa_work_mail@mail.ru>
2021-11-28 19:00:00 +0300

    Update lab6_2.py

--- lab6_2.py
+++ lab6_2.py
@@ -1,19 +1,27 @@
-#   3. Реализовать класс «Человек», включающий в себя имя, фамилию, отчество,
-# год рождения и методы, позволяющие изменять/получать значения этих полей.
-# Реализовать производные классы:
-#   «Преподаватель университета» с полями: должность, ученая степень, специальность,
-# список научных трудов (массив строк);
-#   «Член комиссии» с полями: название комиссии, год назначения в комиссию,
-# номер свидетельства, автобиография (массив строк);
-#   «Преподаватели – члены комиссии» (производный от 2 и 3).
-# Дополнительное поле – список работ выполненных в комиссии.
-# Классы должны содержать методы доступа и изменения всех полей.
+"""
+4.  Реализовать класс «Человек», включающий в себя имя, фамилию, отчество,
+год рождения и методы, позволяющие изменять/получать значения этих полей.
+Реализовать производные классы:
+    «Предприниматель» - содержит номер лицензии, адрес регистрации,
+УНН, данные о налоговых платежах (массив пар вида <дата, сумма>).
+    «Турист» - содержит данные паспорта(строка), данные о пересечении границы
+в виде массива пар <дата, страна>.
+    «Челнок» (производный от 2 и 3) – добавляется массив строк – список адресов,
+по которым покупается товар. Классы должны содержать методы доступа и изменения всех полей.
+"""
 
 from abc import ABC, abstractmethod
 
-class Human(ABC):
 
+class Human(ABC):
+    """
+    Kласс «Человек», включающий в себя имя, фамилию, отчество,
+    год рождения и методы, позволяющие изменять/получать значения этих полей.
+    """
     def __init__(self, name, surname, patronymic, year_born):
+        """
+        Инициализация класса «Человек»
+        """
         self.name = name
         self.surname = surname
         self.patronymic = patronymic
@@ -21,106 +29,148 @@ class Human(ABC):
 
     @abstractmethod
     def print_info(self):
-        pass
+        """
+        Получение информации об экземпляре класса
+        """
 
     @abstractmethod
     def change_info(self):
-        pass
+        """
+        Изменение информации об экземпляре класса
+        """
 
-class Univer_lecturer(Human):
 
-    def __init__(self, name = None, surname = None, patronymic = None, year_born = None, position = None, academ_degree = None, speciality = None, work_list = None):
+class Entrepreneur(Human):
+    """
+    Kласс «Предприниматель» - содержит номер лицензии, адрес регистрации,
+    УНН, данные о налоговых платежах (массив пар вида <дата, сумма>).
+    """
+    def __init__(
+            self, name=None, surname=None, patronymic=None, year_born=None,
+            license_number=None, registration_address=None, unn=None, data_on_tax_payments=None    # unn - это (УНН)
+    ):
         super().__init__(name, surname, patronymic, year_born)
-        self.position = position
-        self.academ_degree = academ_degree
-        self.speciality = speciality
-        self.work_list = work_list
-
-
-    def print_info(self):
-        print('\n' + self.name + ' ' + self.surname + ' ' + self.patronymic + ' ' + self.year_born + ' года рождения ' + \
-              ' \nДолжность: ' + self.position + ' \nУченая степень: ' + self.academ_degree + \
-              ' \nСпециальность: ' + self.speciality + ' \nСписок научных трудов: ' + str(self.work_list))
-
-    def change_info(self):
-        self.name = input("Введите имя преподавателя: ")
-        self.surname = input("Введите фамилию преподавателя: ")
-        self.patronymic = input("Введите отчество преподавателя: ")
-        self.year_born = input("Введите дату рождния: ")
-        self.position = input("Введите должность: ")
-        self.academ_degree = input("Введите ученую степень: ")
-        self.speciality = input("Введите специальность: ")
-        self.work_list = input("Введите список научных работ: ")
-        return self.print_info()
-
-class Commis_member(Human):
-    def __init__(self, name = None, surname = None, patronymic = None, year_born = None, commis_name = None, commis_appoint_year = None, certificate_number = None, autobiogr = None):
-        super().__init__(name, surname, patronymic, year_born)
-        self.commis_name = commis_name
-        self.commis_appoint_year = commis_appoint_year
-        self.certificate_number = certificate_number
-        self.autobiogr = autobiogr
-
-
-    def print_info(self):
-        print('\n' + self.name + ' ' + self.surname + ' ' + self.patronymic + ' ' + self.year_born + ' года рождения ' + \
-              ' \nНазвание комиссии: ' + self.commis_name + ' \nГод назначения в комиссию: ' + self.commis_appoint_year +' \nНомер свдельства: ' + self.certificate_number + \
-              ' \nАвтобиография: ' + str(self.autobiogr))
-
-    def change_info(self):
-        self.name = input("Введите имя преподавателя: ")
-        self.surname = input("Введите фамилию преподавателя: ")
-        self.patronymic = input("Введите отчество преподавателя: ")
-        self.year_born = input("Введите дату рождния: ")
-        self.commis_name = input("Введите название комиссии: ")
-        self.commis_appoint_year = input("Введите год назначения в комиссию: ")
-        self.certificate_number = input("Введите номер свидетельства: ")
-        self.autobiogr = input("Введите автобиографию: ")
-        return self.print_info()
-
-class Commis_and_lecturer(Commis_member, Univer_lecturer):
-    def __init__(self, name = None, surname = None, patronymic = None, year_born = None, position = None, academ_degree = None, speciality = None, work_list = None, commis_name = None, commis_appoint_year = None, certificate_number = None,
-                 autobiogr = None, works_in_comis = None):
-        super(Commis_member, self).__init__(name, surname, patronymic, year_born, position, academ_degree, speciality, work_list)
-        self.commis_name = commis_name
-        self.commis_appoint_year = commis_appoint_year
-        self.certificate_number = certificate_number
-        self.autobiogr = autobiogr
-        self.works_in_comis = works_in_comis
+        self.license_number = license_number
+        self.registration_address = registration_address
+        self.unn = unn   # unn (УНН) Учетный номер налогоплательщика
+        self.data_on_tax_payments = data_on_tax_payments
 
     def print_info(self):
         print(
-            '\n' + self.name + ' ' + self.surname + ' ' + self.patronymic + ' ' + self.year_born + ' года рождения ' + \
-            ' \nДолжность: ' + self.position + ' \nУченая степень: ' + self.academ_degree + ' \nСпециальность: ' + self.speciality + \
-            ' \nСписок научных трудов: ' + str(self.work_list) + \
-            ' \nНазвание комиссии: ' + self.commis_name + ' \nГод назначения в комиссию: ' + self.commis_appoint_year + ' \nНомер свдельства: ' + self.certificate_number + \
-            ' \nАвтобиография: ' + str(self.autobiogr) + ' \nСписок работ в комисии: ' + str(self.works_in_comis))
+            f"""\n{self.name} {self.surname} {self.patronymic} {self.year_born} года рождения \
+            \nНомер лицензии: {self.license_number} \
+            \nАдрес регистрации: {self.registration_address}\
+            \nУНН: {self.unn} \
+            \nДанные о налоговых платежах: {str(self.data_on_tax_payments)}
+        """
+        )
+
+    def change_info(self):
+        self.name = input("Введите имя предпринимателя: ")
+        self.surname = input("Введите фамилию предпринимателя: ")
+        self.patronymic = input("Введите отчество предпринимателя: ")
+        self.year_born = input("Введите дату рождния: ")
+        self.license_number = input("Введите номер лицензии ")
+        self.registration_address = input("Введите адрес регистрации: ")
+        self.unn = input("Введите УНН: ")
+        self.data_on_tax_payments = input("Введите данные о налоговых платежах: ")
+        return self.print_info()
+
+
+class Tourist(Human):
+    """
+    Kласс «Турист» - содержит данные паспорта(строка),
+    данные о пересечении границы в виде массива пар <дата, страна>.
+    """
+    def __init__(
+            self, name=None, surname=None, patronymic=None,
+            year_born=None, passport_data=None, border_crossing_data=None
+    ):
+        super().__init__(name, surname, patronymic, year_born)
+        self.passport_data = passport_data
+        self.border_crossing_data = border_crossing_data
+
+    def print_info(self):
+        print(
+            f"""\n{self.name} {self.surname} {self.patronymic} {self.year_born} года рождения \
+            \nПаспортные данные: {self.passport_data} \
+            \nДанные о пересечении границы: {self.border_crossing_data}
+        """
+        )
+
+    def change_info(self):
+        self.name = input("Введите имя туриста: ")
+        self.surname = input("Введите фамилию туриста: ")
+        self.patronymic = input("Введите отчество туриста: ")
+        self.year_born = input("Введите дату рождния: ")
+        self.passport_data = input("Введите паспортные данные: ")
+        self.border_crossing_data = input("Введите данные о пересечении границы: ")
+        return self.print_info()
+
+
+class Shuttle(Tourist, Entrepreneur):
+    """
+    «Челнок» (производный от 2 и 3) – добавляется массив строк – список адресов,
+    по которым покупается товар. Классы должны содержать методы доступа и изменения всех полей.
+    """
+    def __init__(
+            self, name=None, surname=None, patronymic=None, year_born=None, license_number=None,
+            registration_address=None, unn=None, data_on_tax_payments=None,
+            passport_data=None, border_crossing_data=None, product_purchase_addresses=None
+    ):
+        super(Tourist, self).__init__(
+            name, surname, patronymic, year_born,
+            license_number, registration_address, unn, data_on_tax_payments
+        )
+        self.passport_data = passport_data
+        self.border_crossing_data = border_crossing_data
+        self.product_purchase_addresses = product_purchase_addresses
+
+    def print_info(self):
+        print(
+            f"""\n{self.name} {self.surname} {self.patronymic} {self.year_born} года рождения \
+            \nНомер лицензии: {self.license_number} \
+            \nАдрес регистрации: {self.registration_address}\
+            \nУНН: {self.unn} \
+            \nДанные о налоговых платежах: {str(self.data_on_tax_payments)}\
+            \nПаспортные данные: {self.passport_data} \
+            \nДанные о пересечении границы: {self.border_crossing_data}
+        """
+        )
 
     def change_info(self):
         self.name = input("Введите имя преподавателя: ")
         self.surname = input("Введите фамилию преподавателя: ")
         self.patronymic = input("Введите отчество преподавателя: ")
         self.year_born = input("Введите дату рождния: ")
-        self.position = input("Введите должность: ")
-        self.academ_degree = input("Введите ученую степень: ")
-        self.speciality = input("Введите специальность: ")
-        self.work_list = input("Введите список научных работ: ")
-        self.commis_name = input("Введите название комиссии: ")
-        self.commis_appoint_year = input("Введите год назначения в комиссию: ")
-        self.certificate_number = input("Введите номер свидетельства: ")
-        self.autobiogr = input("Введите автобиографию: ")
-        self.works_in_comis = input("Введите список работ в комиссии: ")
+        self.license_number = input("Введите номер лицензии ")
+        self.registration_address = input("Введите адрес регистрации: ")
+        self.unn = input("Введите УНН: ")
+        self.data_on_tax_payments = input("Введите данные о налоговых платежах: ")
+        self.passport_data = input("Введите паспортные данные: ")
+        self.border_crossing_data = input("Введите данные о пересечении границы: ")
         return self.print_info()
 
 
+entrepreneur = Entrepreneur(
+    'Васильев', 'Василий', 'Васильевич', '1970', '700500300', 'Минск, ул. Ленина, д. 1',
+    '946134589', {'2018': 10000, '2019': 20000, '2020': 50000}
+)
+entrepreneur.print_info()
+entrepreneur.change_info()
 
-
-lecturer = Univer_lecturer('Васильев', 'Василий', 'Васильевич', '1970', 'Лектор', 'Доцент', 'Математиеское моделирование', ['Математика', 'Cложная математика', 'Cложная математика 2'])
-lecturer.print_info()
-lecturer.change_info()
-
-member = Commis_member('Васильев', 'Василий', 'Васильевич', '1970', '"Комиссия экзаменационная"', '2000', '70055036', ['Оценка энакзамена', 'Формирование другой комиссии'])
+member = Tourist(
+    'Васильев', 'Василий', 'Васильевич', '1970', '4FG6BF4HG61B6C4CV1B',
+    {'12.08.2018': 'Польша', '22.01.2019': 'Германия', '5.010.2020': 'Австрия'}
+)
 member.print_info()
 
-member_lecturer = Commis_and_lecturer('Васильев', 'Василий', 'Васильевич', '1970', '"Комиссия экзаменационная"', '2000', '70055036', ['Физика', 'Cложная физика', 'Cложная физика 2'], 'Лектор', 'Доцент', 'Математиеское моделирование', ['Оценка зачета', 'Формирование третьей комиссии'], 'Родился, учился, преподает')
+member_lecturer = Shuttle(
+    'Васильев', 'Василий', 'Васильевич', '1970', '700500300', 'Минск, ул. Ленина, д. 1',
+    '946134589', {'2018': 10000, '2019': 20000, '2020': 50000},
+    {'12.08.2018': 'Польша', '22.01.2019': 'Германия', '5.010.2020': 'Австрия'},
+    ['Ул. Ленина, 3', 'Ул. Пушкина, 14', 'Ул. Гоголя, 59']
+)
+member_lecturer.print_info()
+member_lecturer.change_info()
 member_lecturer.print_info()
