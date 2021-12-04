5d8e3b81edc3e21a4bcffd1649ceed02a63486c4
Andrey <alfa_work_mail@mail.ru>
2021-12-04 18:39:32 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -5,7 +5,7 @@
 
 class Tomato:
 
-    # Стадии созревания помидора
+    # Стадии созревания помидора 
     states = {0: 'nothing', 1: 'flower', 2: 'green_tomato', 3: 'red_tomato'}
 
     def __init__(self, index):
@@ -76,3 +76,33 @@ class Gardener:
             print('Too early! Your plant is green and not ripe.')
 
 
+class switch(object):
+    def __init__(self, value):
+        self.value = value  # значение, которое будем искать
+        self.fall = False   # для пустых case блоков
+
+    def __iter__(self):     # для использования в цикле for
+        """ Возвращает один раз метод match и завершается """
+        yield self.match
+
+    def match(self, *args):
+        """ Указывает, нужно ли заходить в тестовый вариант """
+        if self.fall or not args:
+            # пустой список аргументов означает последний блок case
+            # fall означает, что ранее сработало условие и нужно заходить
+            #   в каждый case до первого break
+            return True
+        elif self.value in args:
+            self.fall = True
+            return True
+        return False
+
+
+if __name__ == '__main__':
+    great_tomato_bush = TomatoBush(4)
+    gardener = Gardener('Emilio', great_tomato_bush)
+    gardener.work()
+    gardener.work()
+    gardener.harvest()
+    gardener.work()
+    gardener.harvest()
